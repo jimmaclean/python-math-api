8f4333e1257ce5e4621e2238835f6d4cc759b2ee
jimmaclean <jqim87@gmail.com>
2020-12-01 20:22:08 +0000

    simple return number

--- handler.py
+++ handler.py
@@ -14,11 +14,13 @@ def hello(event, context):
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+
+def square(event, context):
+    x = int(event["queryStringParameters"]['int'])
+    
+    response = {
+        "statusCode": 200,
+        "body": x * x
     }
-    """
+
+    return response
